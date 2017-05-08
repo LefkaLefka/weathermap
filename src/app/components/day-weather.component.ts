a8018ec88134f30da4ff21196d2c84b6d411c8e2
LefkaLefka <lefka.velchenko@gmail.com>
2017-05-08 22:02:15 +0300

    Style for chart/Some changes with chart options

--- src/app/components/day-weather.component.ts
+++ src/app/components/day-weather.component.ts
@@ -19,10 +19,12 @@ export class DayWeatherComponent {
 
             this.bubbleChartOptions.hAxis.minValue = new Date((buffData[0].time - 3600) * 1000);
             this.bubbleChartOptions.hAxis.maxValue = new Date((buffData[24].time + 3600) * 1000);
+            this.bubbleChartOptions.vAxis.maxValue = -100;
+            this.bubbleChartOptions.vAxis.minValue = 100;
 
             for (let i = 0; i < 25; ++i) {
                 let buffArray = [];
-                buffArray.push(buffData[i].apparentTemperature.toString() + " °C");
+                buffArray.push(buffData[i].apparentTemperature.toString().slice(0, -3) + " °C");
                 buffArray.push(new Date(buffData[i].time * 1000));
                 buffArray.push(buffData[i].apparentTemperature);
                 buffArray.push(buffData[i].summary);
@@ -31,10 +33,10 @@ export class DayWeatherComponent {
                 this._bubbleChartData.push(buffArray);
 
                 if(buffData[i].apparentTemperature > this.bubbleChartOptions.vAxis.maxValue) {
-                    this.bubbleChartOptions.vAxis.maxValue = buffData[i].apparentTemperature + 5;
+                    this.bubbleChartOptions.vAxis.maxValue = buffData[i].apparentTemperature + 10;
                 }
                 if(buffData[i].apparentTemperature < this.bubbleChartOptions.vAxis.minValue) {
-                    this.bubbleChartOptions.vAxis.minValue = buffData[i].apparentTemperature - 5;
+                    this.bubbleChartOptions.vAxis.minValue = buffData[i].apparentTemperature - 10;
                 }
             }
         }
@@ -45,12 +47,19 @@ export class DayWeatherComponent {
     }
 
     public bubbleChartOptions = {
-        title: "24hr temperature",
+        backgroundColor: "#F3F1ED",
+        title: "",
+        chartArea: {
+            left: "5%",
+            right: "10%",
+            width: "100%",
+            backgroundColor: "#FAFAD2"
+        },
         hAxis: {
             minValue: null,
             maxValue: null,
             gridlines: {
-                count: 25,
+                count: 12
             }
         },
         vAxis: {
